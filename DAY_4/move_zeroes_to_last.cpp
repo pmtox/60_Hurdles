a2bae94d9a839f6ec8aad5c3dbcd2501e9d803d7
pmtox <pmishraa24@gmail.com>
2026-01-06 23:43:40 +0530

    Added optimal for move zeroes to the last

--- DAY_4/move_zeroes_to_last.cpp
+++ DAY_4/move_zeroes_to_last.cpp
@@ -6,19 +6,43 @@ int main(){
 	for(int i = 0 ; i < n ; i++){
 		cin>>arr[i];
 	}
-	int swapped;
-    for (int i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] == 0) {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
-            }
-        }
-        if (!swapped)
-            break;
-    }
+//	int swapped;
+//    for (int i = 0; i < n - 1; i++) {
+//        swapped = false;
+//        for (int j = 0; j < n - i - 1; j++) {
+//            if (arr[j] == 0) {
+//                swap(arr[j], arr[j + 1]);
+//                swapped = true;
+//            }
+//        }
+//        if (!swapped)
+//            break;
+
+
+	int j = -1;
+	for(int i = 0 ; i < n ; i++){
+		if(arr[i] == 0){
+			j = i;
+			break;
+		}
+	}
+	
+	if(j == -1) {
+		for(int it = 0; it<n;it++){
+			cout<<arr[it] <<" ";
+		}
+		return 0;
+	}
+	
+	for(int i = j+1 ; i < n ; i++){
+		if(arr[i] != 0){
+			swap(arr[i] , arr[j]);
+			j++;
+		}
+	}
+	
 	for(int it = 0; it<n;it++){
 		cout<<arr[it] <<" ";
 	}
+	return 0;
 }
