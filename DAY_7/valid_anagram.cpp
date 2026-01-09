77471873c5fc1f29b2643466a0cf85ec711ddaa1
pmtox <pmishraa24@gmail.com>
2026-01-10 00:40:05 +0530

    DAY 7 Updated

--- DAY_7/valid_anagram.cpp
+++ DAY_7/valid_anagram.cpp
@@ -1,4 +1,4 @@
-
+//APPROACH - I
 
 class Solution {
 public:
@@ -9,5 +9,26 @@ public:
             return true;
         }
         return false;
+    }
+};
+
+//APPROACH - II
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char , int> cntS;
+        unordered_map<char , int> cntT;
+
+        for(int it= 0 ; it< s.size() ; it++){
+            cntS[s[it]]++;
+        }
+        for(int it= 0 ; it < t.size() ; it++){
+            cntT[t[it]]++;
+        }
+        return cntS == cntT;
     }
 };
