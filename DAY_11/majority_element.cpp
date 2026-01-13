e95ab87410c810818b210869160f4bb6e89cb189
pmtox <pmishraa24@gmail.com>
2026-01-13 20:05:16 +0530

    DAY_11 UPDATED

---
+++ DAY_11/majority_element.cpp
@@ -0,0 +1,41 @@
+//APPROACH - I
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int , int> mpp;
+        for(int i =0; i< n ; i++){
+            mpp[nums[i]]++;
+        }
+        n = n/2;
+        for(auto it : mpp){
+            if(it.second>n){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
+
+//APPROACH - II
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        for(int i =0; i< n ; i++){
+            int count = 0;
+            sort(nums.begin() , nums.end());
+            for(int j =0; j< n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count > n/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
--- DAY_11/revision_day/revision_SHEET2.cpp
+++ DAY_11/revision_day/revision_SHEET2.cpp
@@ -54,3 +54,62 @@ int main(){
     cout<<"NO NON REPEATING ELEMENT";
     return 0;
 }
+
+
+// LONGEST COMMON PREFIX
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        for(int i=0; i< strs[0].size(); i++){
+            char ch = strs[0][i];
+            for(int j =0; j< strs.size(); j++){
+                if(i >= strs[j].size() || ch != strs[j][i]){
+                    return strs[0].substr(0 , i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
+
+
+// GROUP ANAGRAMS
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>st;
+        unordered_map<string , vector<string>>mpp;
+        for(const auto& it : strs){
+            string s = it;
+            sort(s.begin() , s.end());
+            mpp[s].push_back(it);
+
+        }
+        for(auto &pair : mpp ){
+            st.push_back(pair.second);
+        }
+        return st;
+    }
+};
+
+
+
+// Remove Element
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> temp;
+        for(int i =0; i< nums.size(); i++){
+            if(nums[i] != val){
+                temp.push_back(nums[i]);
+            }
+        }
+        for(int i =0; i< temp.size() ; i++){
+            nums[i] = temp[i];
+        }
+        return temp.size();
+    }
+};
