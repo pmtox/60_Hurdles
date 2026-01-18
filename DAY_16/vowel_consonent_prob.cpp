d2a8b20e653eb0740c2159f357457653e44f2800
pmtox <pmishraa24@gmail.com>
2026-01-18 18:33:43 +0530

    DAY 16 UPDATED

---
+++ DAY_16/vowel_consonent_prob.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int v = 0;
+        int c = 0;
+        for(char ch : s){
+            ch = tolower(ch);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch== 'u'){
+                v++;
+            }else if(ch >= 'a' && ch <= 'z'){
+                c++;
+            }
+        }
+        if(c>0){
+            return v/c;
+        }
+        return 0;
+    }
+};
