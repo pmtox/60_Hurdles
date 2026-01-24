3bbd15ddf2f1eed54ffa68dd368991d39848589e
pmtox <pmishraa24@gmail.com>
2026-01-24 21:07:08 +0530

    day 22 initialized

--- DAY_22/set_matrix_zero.cpp
+++ DAY_22/set_matrix_zero.cpp
@@ -23,3 +23,82 @@ public:
         }
     }
 };
+
+// APPROACH-II
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> mat = matrix;
+        for(int i =0; i< n ; i++){
+            for(int j = 0; j< m ; j++){
+                if(matrix[i][j] == 0){
+                    for(int k =0; k < m ; k++){
+                        mat[i][k] = 0;
+                    }
+                }
+            }
+        }
+        for(int i =0; i< n ; i++){
+           for(int j =0; j< m ; j++){
+            if(matrix[i][j] == 0){
+                for(int k =0; k< n ; k++){
+                    mat[k][j] = 0;
+                }
+            }
+           }
+        }
+        for(int i =0; i< n ;i++){
+            for(int j =0; j<m ; j++){
+                matrix[i][j] = mat[i][j];
+            }
+        }
+    }
+};
+
+
+// APPROACH-III
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int cnt = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i =0; i < n ; i++){
+            for(int j =0; j< m ; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j!= 0){
+                        matrix[0][j] = 0;
+                    }else{
+                        cnt = 0;
+                    }
+                }
+            }
+        }
+        for(int i =1; i< n ; i++){
+            for(int j =1; j< m ; j++){
+                if(matrix[i][j] != 0){
+                      if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] =0;
+                      }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j< m ; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(cnt == 0){
+            for(int i = 0; i< n ; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
