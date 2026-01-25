0c06cec01a288f135bf7443ca32bb482c18d8fb8
pmtox <pmishraa24@gmail.com>
2026-01-26 01:02:51 +0530

    moore's algorithm done

--- DAY_11/majority_element.cpp
+++ DAY_11/majority_element.cpp
@@ -38,3 +38,35 @@ public:
         return -1;
     }
 };
+
+
+
+// MOORE'S ALGORITHM
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int el;
+        int cnt =0;
+        for(int i =0; i< n;i++){
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == el){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        int cnt1 = 0;
+        for(int i =0; i< n ; i++){
+            if(nums[i] == el){
+                cnt1++;
+            }
+        }
+        if(cnt1 > n /2 ) return el;
+        return -1;
+    }
+};
