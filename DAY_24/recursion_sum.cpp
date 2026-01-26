6af029693ef3b71971cdbcdd7cd448389ace5254
pmtox <pmishraa24@gmail.com>
2026-01-26 22:48:24 +0530

    initialized day 26

---
+++ DAY_24/recursion_sum.cpp
@@ -0,0 +1,46 @@
+// SUM USING PARAMETERIZED RECURSION
+
+//#include<iostream>
+//using namespace std;
+//void summ(int i , int sum){
+//    if(i <1){
+//    	cout<<sum;
+//        return;
+//    }
+//    summ(i-1 , sum+i);
+//}
+//int main(){
+//    int n ; cin>>n;
+//    summ(n , 0);
+//}
+
+// SUM USING FUNCTIONAL RECURSION
+
+//#include<iostream>
+//using namespace std;
+//int summ(int n){
+//    if(n == 0){
+//        return 0;
+//    }
+//    return n + summ(n-1);
+//}
+//int main(){
+//    int n ; cin>>n;
+//    cout<<summ(n);
+//}
+
+// FACTORIAL USING RECURSION
+
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+// 	if(n==0) return 1;
+// 	return n * fact(n-1);
+// }
+// int main(){
+// 	int n ; cin>>n;
+// 	cout<<fact(n);
+// }
+
+
+// 
--- DAY_4/move_zeroes_to_last.cpp
+++ DAY_4/move_zeroes_to_last.cpp
@@ -6,7 +6,7 @@ int main(){
 	for(int i = 0 ; i < n ; i++){
 		cin>>arr[i];
 	}
-//	int swapped;
+//	bool swapped;
 //    for (int i = 0; i < n - 1; i++) {
 //        swapped = false;
 //        for (int j = 0; j < n - i - 1; j++) {
