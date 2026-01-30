c148ed16638f3ca7fde7b58d1db62304a33e9db4
pmtox <pmishraa24@gmail.com>
2026-01-31 01:35:48 +0530

    day 28 updated

--- DAY_28/subsequence_using_recursion.cpp
+++ DAY_28/subsequence_using_recursion.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void subSe(vector<int> &arr , int n){
-    if()
+void subSe(vector<int> &arr ,vector<int> &extraa , int n , int ind){
+    if(ind == n){
+        for(auto it : extraa){
+            cout<<it<<" ";
+        }
+        if(extraa.size() == 0){
+            cout<<"{}";
+        }
+        cout<<endl;
+        return;
+    }
+    extraa.push_back(arr[ind]);
+    subSe(arr , extraa , n , ind+1);
+    extraa.pop_back();
+    subSe(arr , extraa , n , ind+1);
 }
 int main(){
     int n; cin>>n;
-    vector<int> arr[n];
+    vector<int> arr(n);
     for(int i =0; i<n;i++){
         cin>>arr[i];
     }
-    subSe(arr , n);
+    vector<int> extraa;
+    subSe(arr ,extraa, n , 0);
+    return 0;
 }
