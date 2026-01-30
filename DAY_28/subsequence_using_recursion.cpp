075973e7cc4e39bd6ba37329f81bf5d8e5e3e4e7
pmtox <pmishraa24@gmail.com>
2026-01-30 23:32:47 +0530

    day 28 initialized

---
+++ DAY_28/subsequence_using_recursion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void subSe(vector<int> &arr , int n){
+    if()
+}
+int main(){
+    int n; cin>>n;
+    vector<int> arr[n];
+    for(int i =0; i<n;i++){
+        cin>>arr[i];
+    }
+    subSe(arr , n);
+}
