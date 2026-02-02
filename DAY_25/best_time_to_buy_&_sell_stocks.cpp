b5f57a4734a04ec9706dc3c7cb77a0f57535570c
pmtox <pmishraa24@gmail.com>
2026-02-03 00:49:11 +0530

    best tym to add stocks

--- DAY_25/best_time_to_buy_&_sell_stocks.cpp
+++ DAY_25/best_time_to_buy_&_sell_stocks.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0; int cnt2 = 0;
-        int el1 = INT_MIN;
-        int el2 = INT_MIN;
-        for(int i =0; i< nums.size(); i++){
-            if(cnt1 == 0 && nums[i] != el2){
-                cnt1 =1;
-                el1 = nums[i];
-            } else if(cnt2 == 0 && nums[i] != el1){
-                cnt2 = 1;
-                el2 = nums[i];
-            } 
-            else if(el1 == nums[i]) cnt1++;
-            else if(el2 == nums[i]) cnt2++;
-            else{
-                cnt1--; cnt2--;
-            }
+    int maxProfit(vector<int>& prices) {
+        int maxpro = 0;
+        int mini = prices[0];
+        for(int i =0; i< prices.size(); i++){
+            int cost = prices[i] - mini;
+            maxpro = max(maxpro , cost);
+            mini = min(prices[i] , mini);
         }
-        vector<int> ans;
-        cnt1 = 0; cnt2 = 0;
-        for(int i =0; i<nums.size(); i++){
-            if(el1 == nums[i]) cnt1++;
-            if(el2 == nums[i]) cnt2++;
-        }
-        int minii = (int)(nums.size()/3) +1;
-        if(cnt1 >= minii) ans.push_back(el1);
-        if(cnt2 >= minii) ans.push_back(el2);
-        sort(ans.begin() , ans.end());
-        return ans;
+        return maxpro;
     }
 };
