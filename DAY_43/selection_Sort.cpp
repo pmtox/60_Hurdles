f702cca87268dcd5cf62818ca13b25350f336380
pmtox <pmishraa24@gmail.com>
2026-02-13 23:37:02 +0530

    day 43 initialized

---
+++ DAY_43/selection_Sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void slctSort(int arr[] , int n){
+    for(int i =0; i< n ; i++){
+        int min_idx = i;
+        for(int j = i+1; j < n ; j++){
+            if(arr[j] < arr[min_idx]){
+            min_idx = j;
+            }
+        }
+        swap(arr[i] , arr[min_idx]);
+    }
+}
+void sortedArr(int arr[] , int n){
+    for(int i =0 ;i < n ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n ; cin>>n;
+    int arr[n];
+    for(int i =0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    slctSort(arr , n);
+    sortedArr(arr , n);
+    return 0;
+}
