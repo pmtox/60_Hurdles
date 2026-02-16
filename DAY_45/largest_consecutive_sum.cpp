8bea6e9a619d66bea4718df6ca693240b20ffa39
pmtox <pmishraa24@gmail.com>
2026-02-16 23:49:34 +0530

    modified

--- DAY_45/largest_consecutive_sum.cpp
+++ DAY_45/largest_consecutive_sum.cpp
@@ -14,12 +14,13 @@ int main(){
     for(int i = 0; i < n ; i++){
         cin>>arr[i];
     }
-    int currSum = getSum(arr , 0 , n-1);
+    int k; cin>>k;
+    int currSum = getSum(arr , 0 , k);
     int larSum = currSum;
 
-    for(int i = 0 ; i <= n ; i++){
+    for(int i = 1 ; i <= n-k ; i++){
         currSum -= arr[i-1];
-        currSum += arr[i+1];
+        currSum += arr[i + k - 1];
         larSum = max(currSum , larSum);
     }
     cout<<larSum << " ";
