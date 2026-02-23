ae40651fa60f939f9c560e9bcaa2e7c491d2bca5
pmtox <pmishraa24@gmail.com>
2026-02-24 00:00:54 +0530

    question done

---
+++ DAY_53/Parkour_design.cpp
@@ -0,0 +1,31 @@
+// CodeForces Contest Question
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void solve() {
+        long long x, y;
+        cin >> x >> y;
+
+        if (x - 2*y >= 0 && (x - 2*y) % 3 == 0 && x + 4*y >= 0) {
+            cout << "YES\n";
+        }else {
+            cout << "NO\n";
+        }
+    }
+};
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    Solution obj;
+    while (t--) {
+        obj.solve();
+    }
+
+    return 0;
+}
